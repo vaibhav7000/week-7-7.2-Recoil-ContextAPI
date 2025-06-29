a12561027fe49f59a8f1509aef4f30ee9a9ad93b
vaibhav chawla <vaibhav.chawla@roundglass.com>
2025-06-29 16:45:31 +0530

    some concept clearing of contextAPI

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,54 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createContext } from 'react'
+import { useContext } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log("app")
+  return (
+    <Child/>
+  )
+}
+
+// creating context
+const CountContext = createContext();
+
+// creating wrapper for context
+export function CountProvider({ children }) {
+  const [count, setCount] = useState(0);
 
   return (
+    <CountContext.Provider value={
+      {count, setCount}
+    }>
+      { children }
+    </CountContext.Provider>
+  )
+}
+
+function Child() {
+  console.log("child");
+  return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GChild/>
+      <GChild/>
     </>
   )
 }
 
+
+function GChild() {
+  const { count, setCount } = useContext(CountContext);
+  console.log("GChild");
+  return (
+    <button onClick={() => setCount(previous => previous + 1)}>
+      {count}
+    </button>
+  )
+}
+
 export default App
+
+
+// Component that does not uses values from the contextAPI will not get re-rendered. Only those component will re-render will re-render that uses any value from the component
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CountProvider } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <CountProvider>
     <App />
-  </StrictMode>,
+  </CountProvider>,
 )
